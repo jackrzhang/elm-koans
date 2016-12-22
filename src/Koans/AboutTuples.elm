d3a803048efaaa3378fc165443afd207b3f0d318
Jack Zhang <15jzhang@gmail.com>
2016-12-21 20:24:49 -0500

    Pass AboutTuples

--- src/Koans/AboutTuples.elm
+++ src/Koans/AboutTuples.elm
@@ -10,37 +10,39 @@ testSuite =
     describe "About Tuples"
         [ test "tuples are like lists of fixed length" <|
             \() ->
-                xTuple
+                ( 1, 2 )
                     |> Expect.equal ( 1, 2 )
         , test "tuples may also be of mixed types" <|
             \() ->
-                xTuple2
+                ( 1, "hey" )
                     |> Expect.equal ( 1, "hey" )
         , test "there is a special comma syntax for creating tuples" <|
             \() ->
-                xTuple2
+                (,) 1 "hey"
                     |> Expect.equal ((,) 1 "hey")
+                    -- <function> : a -> b -> ( a, b )
         , test "you use as many commas as there would be in the tuple" <|
             \() ->
-                xTuple3
+                (,,) 1 "hey" []
                     |> Expect.equal ((,,) 1 "hey" [])
+                    -- <function> : a -> b -> c -> ( a, b, c )
         , test "first gets the first element of a 2-tuple" <|
             \() ->
-                xNum
+                100
                     |> Expect.equal (Tuple.first xTuple2)
         , test "second gets the second element of a 2-tuple" <|
             \() ->
-                xString
+                ""
                     |> Expect.equal (Tuple.second xTuple2)
         , test "case statements may be used to destructure a tuple" <|
             \() ->
                 case ( 1, 2 ) of
                     ( first, second ) ->
-                        ((first == xNum) && (second == xNum))
+                        ((first == 1) && (second == 2))
                             |> Expect.true "Should be True"
         , test "tuples may also be destructured by function arguments" <|
             \() ->
                 ( 1, 2 )
-                    |> (\( f, s ) -> ((f == xNum) && (s == xNum)))
+                    |> (\( f, s ) -> ((f == 1) && (s == 2)))
                     |> Expect.true "Should be True"
         ]
